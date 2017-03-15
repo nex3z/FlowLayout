3f4126787cc1b71f7a196a198e74db8b5f20051b
Tianxing Li <litianxing9@gmail.com>
2017-03-16 00:08:58 +0800

    Add test for FlowLayout with RecyclerView.

---
+++ sample/src/main/java/com/nex3z/flowlayout/sample/RecyclerViewActivity.java
@@ -0,0 +1,25 @@
+package com.nex3z.flowlayout.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        SimpleAdapter adapter = new SimpleAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setCount(10);
+    }
+}
